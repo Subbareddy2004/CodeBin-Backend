399f485df254b3aadbad2f4c7de3cfa55ce19610
Tirumalareddysai <tirumalareddysai2004@gmail.com>
2024-10-23 21:19:37 +0530

    route issue

--- config/database.js
+++ config/database.js
@@ -1,10 +1,8 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
-const pg = require('pg');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  dialectModule: pg,
   dialectOptions: {
     ssl: {
       require: true,
